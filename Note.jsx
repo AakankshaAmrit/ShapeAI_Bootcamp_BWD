4b7180b64bd83c08c9bb9a98debb9239a7ef491f
AakankshaAmrit <85403153+AakankshaAmrit@users.noreply.github.com>
2021-06-27 15:03:31 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Data Warehouses</h1>
+      <p>
+        A data warehouse is a repository of information collected from multiple
+        sources, stored under a unified schema, and usually residing at a single
+        site. Data warehouses are constructed via a process of data cleaning,
+        data integration, data transformation, data loading, and periodic data
+        refreshing.
+      </p>
+    </div>
+  );
+}
+export default Note;
